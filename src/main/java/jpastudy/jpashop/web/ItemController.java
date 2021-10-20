1757a5a0d3c6231f70cda537770cf1a5d254c156
mysoyul <vega2k_@naver.com>
2021-10-20 17:47:11 +0900

    Item등록처리

---
+++ src/main/java/jpastudy/jpashop/web/ItemController.java
@@ -0,0 +1,41 @@
+package jpastudy.jpashop.web;
+
+import jpastudy.jpashop.domain.item.Book;
+import jpastudy.jpashop.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+    private final ItemService itemService;
+
+    @GetMapping(value = "/items/new")
+    public String createForm(Model model) {
+        model.addAttribute("bookForm", new BookForm());
+        return "items/createItemForm";
+    }
+
+    @PostMapping(value = "/items/new")
+    public String create(@Valid BookForm form, BindingResult result) {
+        if (result.hasErrors()) {
+            return "items/createItemForm";
+        }
+        Book book = new Book();
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+        itemService.saveItem(book);
+        //return "redirect:/items";
+        return "redirect:/";
+    }
+
+}

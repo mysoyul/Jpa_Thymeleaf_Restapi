4616ab4fb5bb4f13f35c4177dc90e8d57904dd5e
mysoyul <vega2k_@naver.com>
2021-10-20 09:17:36 +0900

    JPA활용 타임리프와RESTAPI

---
+++ src/main/java/jpastudy/jpashop/repository/MemberRepository.java
@@ -0,0 +1,36 @@
+package jpastudy.jpashop.repository;
+
+import jpastudy.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberRepository {
+    //@PersistenceContext
+    //@Autowired 대체가능
+    private final EntityManager em;
+
+    //등록
+    public void save(Member member) {
+        em.persist(member);
+    }
+    //id로 Member 하나 조회
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);
+    }
+    //Member 모두 조회
+    public List<Member> findAll() {
+        //TypedQuery
+        return em.createQuery("select m from Member m", Member.class).getResultList();
+    }
+    //name으로 Member 하나 또는 여러개 조회
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name) //TypedQuery
+                .getResultList();
+    }
+}

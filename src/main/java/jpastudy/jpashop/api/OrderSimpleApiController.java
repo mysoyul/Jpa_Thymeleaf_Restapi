d03a45c4b4e4585cc3d258a9fa9c4bd6bbb464cb
mysoyul <vega2k_@naver.com>
2021-10-21 16:53:33 +0900

    Fetch Join 사용

--- src/main/java/jpastudy/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpastudy/jpashop/api/OrderSimpleApiController.java
@@ -1,13 +1,19 @@
 package jpastudy.jpashop.api;
 
+import jpastudy.jpashop.domain.Address;
 import jpastudy.jpashop.domain.Order;
 import jpastudy.jpashop.domain.OrderSearch;
+import jpastudy.jpashop.domain.OrderStatus;
 import jpastudy.jpashop.repository.OrderRepository;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 /**
  * xToOne(ManyToOne, OneToOne) 관계 최적화
@@ -27,4 +33,52 @@ public class OrderSimpleApiController {
         });
         return all;
     }
+
+    /**
+     * V2 : 엔티티를 DTO로 변환
+     * 문제점 : 지연로딩(lazy loading)으로 쿼리 N번 호출
+     * N + 1 문제
+     * Order2건 Member 2번, Delivery 2번
+     * order 1, member 2 , delivery 2
+     */
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAll(new OrderSearch());
+        return  orders.stream() //Stream<Order>
+                .map(order -> new SimpleOrderDto(order))   //Stream<SimpleOrderDto>
+                .collect(toList()); //List<SimpleOrderDto>
+    }
+
+    /**
+     * V3. 엔티티를 조회해서 DTO로 변환(fetch join 사용함)
+     * fetch join으로 쿼리 1번 호출
+     */
+    @GetMapping("/api/v3/simple-orders")
+    public List<SimpleOrderDto> ordersV3() {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery();
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(toList());
+        return result;
+    }
+
+
+    //----------응답과 요청에 사용할 DTO Inner Class 선언
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName(); //Lazy 강제 초기화
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress(); //Lazy 강제 초기화
+        }
+    }
+
 }
